5e39c9af12d1a0a86e3581ff1da129bc1e669787
masnun-siam <siam.cse.bsmrstu@gmail.com>
2020-05-21 16:08:11 +0600

    Commented device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
-import 'package:corona_tracker/splash_screen.dart';
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:corona_tracker/splash_screen.dart';
+// import 'package:device_preview/device_preview.dart';
+// import 'package:flutter/foundation.dart';
 
-void main() {
-  runApp(
-    DevicePreview(
-      builder: (context) => MyApp(),
-      enabled: !kReleaseMode,
-    ),
-  );
-}
+void main() => runApp(MyApp());
+
+// void main() {
+//   runApp(
+//     DevicePreview(
+//       builder: (context) => MyApp(),
+//       enabled: !kReleaseMode,
+//     ),
+//   );
+// }
 
 class MyApp extends StatelessWidget {
   @override
